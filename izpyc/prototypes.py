44aff420d983b7b3b269d28986e945c3c28400c1
tofjw <tttfjw@gmail.com>
2021-02-28 15:58:56 +0900

    add explicit declaration of IZBOOL

--- izpyc/prototypes.py
+++ izpyc/prototypes.py
@@ -10,19 +10,25 @@ def setup(izlib):
         izlib.__getattr__(f).argtype = argtype
         izlib.__getattr__(f).restype = restype
 
-    x("cs_Le", [c_void_p, c_void_p], c_char)
-    x("cs_Lt", [c_void_p, c_void_p], c_char)
-    x("cs_Ge", [c_void_p, c_void_p], c_char)
-    x("cs_Gt", [c_void_p, c_void_p], c_char)
-    x("cs_Eq", [c_void_p, c_void_p], c_char)
-    x("cs_Neq", [c_void_p, c_void_p], c_char)
+    izbool_t = c_char
+    
+    x("cs_Le", [c_void_p, c_void_p], izbool_t)
+    x("cs_Lt", [c_void_p, c_void_p], izbool_t)
+    x("cs_Ge", [c_void_p, c_void_p], izbool_t)
+    x("cs_Gt", [c_void_p, c_void_p], izbool_t)
+    x("cs_Eq", [c_void_p, c_void_p], izbool_t)
+    x("cs_Neq", [c_void_p, c_void_p], izbool_t)
 
-    x("cs_LE", [c_void_p, c_int], c_char)
-    x("cs_LT", [c_void_p, c_int], c_char)
-    x("cs_GE", [c_void_p, c_int], c_char)
-    x("cs_GT", [c_void_p, c_int], c_char)
-    x("cs_EQ", [c_void_p, c_int], c_char)
-    x("cs_NEQ", [c_void_p, c_int], c_char)
+    x("cs_LE", [c_void_p, c_int], izbool_t)
+    x("cs_LT", [c_void_p, c_int], izbool_t)
+    x("cs_GE", [c_void_p, c_int], izbool_t)
+    x("cs_GT", [c_void_p, c_int], izbool_t)
+    x("cs_EQ", [c_void_p, c_int], izbool_t)
+    x("cs_NEQ", [c_void_p, c_int], izbool_t)
+
+    x("cs_isIn", [c_void_p, c_int], izbool_t)
+    x("cs_isFree", [c_void_p], izbool_t)
+    x("cs_isInstantiated", [c_void_p], izbool_t)
 
     izlib.cs_createCSint.restype = c_void_p
     izlib.cs_getVersion.restype = c_char_p
@@ -30,9 +36,7 @@ def setup(izlib):
     izlib.cs_getMax.argtypes = [c_void_p]
 
     # izlib.cs_search.argtypes = [c_c]
-    izlib.cs_search.restype = c_char
-    izlib.cs_isFree.restype = c_char
-    izlib.cs_isInstantiated.restype = c_char
+    izlib.cs_search.restype = izbool_t
 
     x("cs_ScalProd", [c_void_p, c_void_p, c_int], c_void_p)
 1    
